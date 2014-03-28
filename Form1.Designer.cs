cd3ae9c36c3c6672c10b5b5748f4a56fb3099c39
Jonathan Harris <jharris319@gmail.com>
2014-03-28 15:31:26 -0500

    initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace ShareDrive
+{
+    partial class shareDrive
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(shareDrive));
+            this.username = new System.Windows.Forms.TextBox();
+            this.password = new System.Windows.Forms.TextBox();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // username
+            // 
+            this.username.Location = new System.Drawing.Point(65, 147);
+            this.username.Name = "username";
+            this.username.Size = new System.Drawing.Size(145, 20);
+            this.username.TabIndex = 0;
+            // 
+            // password
+            // 
+            this.password.Location = new System.Drawing.Point(65, 173);
+            this.password.Name = "password";
+            this.password.PasswordChar = '*';
+            this.password.Size = new System.Drawing.Size(145, 20);
+            this.password.TabIndex = 1;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "seal.png");
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(98, 212);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // shareDrive
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.username);
+            this.Name = "shareDrive";
+            this.Text = "shareDrive";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox username;
+        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace ShareDrive
+{
+    public partial class shareDrive : Form
+    {
+        public shareDrive()
+        {
+            InitializeComponent();
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public class NETRESOURCE
+        {
+            public int dwScope;
+            public int dwType;
+            public int dwDisplayType;
+            public int dwUsage;
+            public string LocalName;
+            public string RemoteName;
+            public string Comment;
+            public string Provider;
+        }
+        [DllImport("mpr.dll")]
+        public static extern int WNetAddConnection2(NETRESOURCE netResource, string password, string username, int flags);
+
+        private void Form1_Load(object sender, EventArgs e)
+        {  
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            NETRESOURCE myResource = new NETRESOURCE();
+            myResource.dwScope = 0;
+            myResource.dwType = 0;
+            myResource.dwDisplayType = 0;
+            myResource.LocalName = "h:";
+            myResource.RemoteName = @"\\192.168.1.192\testShare";
+            myResource.dwUsage = 0;
+            myResource.Comment = "";
+            myResource.Provider = "";
+
+            int returnValue = WNetAddConnection2(myResource, password.Text, username.Text, 0);
+            MessageBox.Show(returnValue.ToString());
+        }
+    }
+}
